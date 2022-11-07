8a93b9ff2799482e6473f328f41d6fb25d238360
TIWARISUSHMITA <sushmitatiwari.rewa@gmail.com>
2022-11-07 13:54:47 +0530

    kk

---
+++ two.js
@@ -0,0 +1,19 @@
+
+class Account{
+    min Bal=5000;
+    get_Balance()
+    {
+        console.log("server is down");
+    }
+    cash_Withdrawal()
+    {
+        console.log("plz enter the password");
+    }
+    check_Balance(){
+        console.log("plz enter the pin");
+    }
+}
+let a1=new Account();
+a1.get_Balance();
+a1.cash_Withdrawal();
+a1.check_Balance();
